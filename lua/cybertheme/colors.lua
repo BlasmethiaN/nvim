0f45cd9b503d3c260baeea08d9c36f191d415345
blasmethian <dominako223@gmail.com>
2026-04-01 15:16:45 +0200

    fix: format

--- lua/cybertheme/colors.lua
+++ lua/cybertheme/colors.lua
@@ -14,35 +14,34 @@ local colors = {
 local spec = lush(function(injected_functions)
   local sym = injected_functions.sym
   return {
-    Normal       { bg = colors.bg, fg = colors.yellow }, 
-    
-    CursorLine   { bg = colors.bg.li(5) }, 
-    ColorColumn  { bg = colors.bg.li(3) },
-    
-    Comment      { fg = colors.blue.de(40).da(30), gui = "italic" }, 
-    
-    LineNr       { fg = colors.blue.da(50) },
+    Normal { bg = colors.bg, fg = colors.yellow },
+
+    CursorLine { bg = colors.bg.li(5) },
+    ColorColumn { bg = colors.bg.li(3) },
+
+    Comment { fg = colors.blue.de(40).da(30), gui = "italic" },
+
+    LineNr { fg = colors.blue.da(50) },
     CursorLineNr { fg = colors.cyan, gui = "bold" },
 
-    Pmenu        { bg = colors.bg.li(10), fg = colors.blue.li(20) },
-    PmenuSel     { bg = colors.blue, fg = colors.bg, gui = "bold" },
+    Pmenu { bg = colors.bg.li(10), fg = colors.blue.li(20) },
+    PmenuSel { bg = colors.blue, fg = colors.bg, gui = "bold" },
 
-    Keyword      { fg = colors.yellow_soft, gui = "bold" },
-    Statement    { fg = colors.yellow_soft },
-    Function     { fg = colors.cyan },
-    String       { fg = colors.green },
-    Number       { fg = colors.red.li(10) },
-    Type         { fg = colors.blue.li(10) },
-    Identifier   { fg = colors.yellow },
-    
-    Search       { bg = colors.red.da(20), fg = colors.yellow.li(20) },
-    IncSearch    { bg = colors.yellow, fg = colors.bg },
-    
-    Visual       { bg = colors.blue.da(60) }, -- Subdued blue selection
+    Keyword { fg = colors.yellow_soft, gui = "bold" },
+    Statement { fg = colors.yellow_soft },
+    Function { fg = colors.cyan },
+    String { fg = colors.green },
+    Number { fg = colors.red.li(10) },
+    Type { fg = colors.blue.li(10) },
+    Identifier { fg = colors.yellow },
+
+    Search { bg = colors.red.da(20), fg = colors.yellow.li(20) },
+    IncSearch { bg = colors.yellow, fg = colors.bg },
+
+    Visual { bg = colors.blue.da(60) },       -- Subdued blue selection
 
     WinSeparator { fg = colors.blue.da(40) },
   }
 end)
 
 return spec
-
